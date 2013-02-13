9916ba629a953cfe3424bd85a9933bc59e8e9926
Benjamin Eidelman <beneidel@gmail.com>
2013-02-12 22:07:17 -0300

    fixing bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,4 +2,4 @@ var s=document.createElement('script');
 s.setAttribute('id', 'bookmark-loader');
 s.setAttribute('src', 'https://rawgithub.com/benjamine/bookmark-loader/master/bookmark-loader.js');
 s.setAttribute('data-bookmarks','https://rawgithub.com/benjamine/bookmark-loader/master/bookmarks/example.md');
-document.body.appendChild(script);
+document.body.appendChild(s);
