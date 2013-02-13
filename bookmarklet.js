eece33af6399f78d04a5edbe6c9678e188e4102c
Benjamin Eidelman <beneidel@gmail.com>
2013-02-12 23:41:56 -0300

    fixed bookmarklet.js url

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 var s=document.createElement('script');
 s.setAttribute('id', 'bookmark-loader');
-s.setAttribute('src', 'https://rawgithub.com/benjamine/bookmark-loader/master/bookmark-loader.js');
+s.setAttribute('src', 'http://benjamine.github.com/bookmark-loader/bookmark-loader.js');
 s.setAttribute('data-bookmarks','https://rawgithub.com/benjamine/bookmark-loader/master/bookmarks/example.md');
 document.body.appendChild(s);
